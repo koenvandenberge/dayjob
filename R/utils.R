d51e5a47f9c998150d7036578c49b9e5e70be0cc
koenvandenberge <koen.vandenberge@ugent.be>
2020-04-07 14:39:35 -0700

    add colby

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,20 @@ elementToRowCol <- function(element, nrows, ncols){
 }
 
 
+### color by factor or numeric variable using wesanderson colors
+colby <- function(values){
+  if(is(values, "character")){
+    cols <- as.numeric(as.factor(values))
+    return(cols)
+  }
+  if(is(values, "factor")){
+    cols <- as.numeric(values)
+    return(cols)
+  }
+  if(is(values, "numeric")){
+    pal <- wesanderson::wes_palette("Zissou1", n=12, type="continuous")
+    gg <- Hmisc::cut2(values, g=12)
+    cols <- pal[gg]
+    return(cols)
+  }
+}
