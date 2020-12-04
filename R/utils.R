ddf95a2d7272f7c39f8a98f7349e2975c16fed13
koenvandenberge <koen.vandenberge@ugent.be>
2020-12-04 10:14:22 +0100

    update

--- R/utils.R
+++ R/utils.R
@@ -7,7 +7,7 @@ elementToRowCol <- function(element, nrows, ncols){
 
 
 ### color by factor or numeric variable using wesanderson colors
-colby <- function(values){
+colby <- function(values, g=12){
   if(is(values, "character")){
     cols <- as.numeric(as.factor(values))
     return(cols)
@@ -17,9 +17,18 @@ colby <- function(values){
     return(cols)
   }
   if(is(values, "numeric")){
-    pal <- wesanderson::wes_palette("Zissou1", n=12, type="continuous")
-    gg <- Hmisc::cut2(values, g=12)
+    gg <- Hmisc::cut2(values, g=g)
+    pal <- wesanderson::wes_palette("Zissou1", n=nlevels(gg), type="continuous")
     cols <- pal[gg]
     return(cols)
   }
+  if(is(values, "logical")){
+    cols <- as.numeric(values)+1
+    return(cols)
+  }
+}
+
+## drop NA values from a vector
+dropNA <- function(x){
+  x <- x[!is.na(x)]
 }
