a7a3470793195600b41f42802317ad8dd39bf1b0
Koen Van den Berge <KVande14@its.jnj.com>
2022-12-08 14:34:29 +0100

    allow taking along other colData columns in cellPopulationCounts

--- R/cellPopulationCounts.R
+++ R/cellPopulationCounts.R
@@ -2,6 +2,7 @@ getCellPopulationCounts <- function(sce,
                                     patientVar,
                                     cellTypeVar,
                                     group,
+                                    otherColData=NULL,
                                     format = "long"){
   # sce is a SCE object or data frame
   # patientVar is the variable in colData denoting patients
@@ -11,16 +12,22 @@ getCellPopulationCounts <- function(sce,
   df <- data.frame(patient=colData(sce)[,patientVar],
                    celltype=colData(sce)[,cellTypeVar],
                    group=colData(sce)[,group])
+  colDf <- as.data.frame(colData(sce)[,otherColData])
+  colnames(colDf) <- otherColData
+  df <- cbind(df,colDf)
   require(tidyverse)
   sumDf <- df %>%
     group_by(patient, celltype, group) %>%
-    summarize(nCells = n())
+    dplyr::summarize(nCells = n(),
+                     across()) # across() keeps other column names
 
   if(format == "long"){
     return(sumDf)
   } else if(format == "wide"){
     ## transform to wide format
-    popCounts <- pivot_wider(sumDf, names_from=celltype, values_from=nCells)
+    popCounts <- pivot_wider(sumDf, names_from=celltype,
+                             values_from=nCells,
+                             values_fn = unique)
     popCounts[is.na(popCounts)] <- 0
     return(popCounts)
   } else {stop("format should be either 'long' or 'wide'.")}
